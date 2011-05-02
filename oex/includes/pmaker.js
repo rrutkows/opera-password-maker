381c64d849223d1cf1d62b1b2713ca2d2b0b4433
Rafa? Rutkowski <rrutkowski@gmail.com>
2011-05-02 22:36:04 +0200

    Implemented handling frames and iframes recursively. Fixes issue 4.

--- oex/includes/pmaker.js
+++ oex/includes/pmaker.js
@@ -3,9 +3,36 @@ window.addEventListener("load", function() {
         var password = e.data.password;
         if (!password)
             return;
-        var passwordFields = document.querySelectorAll('input[type="password"]'),
+        fillForms(document, password);
+    }, false);
+    
+    function fillForms(doc, password)
+    {
+        var passwordFields = doc.querySelectorAll('input[type="password"]'),
             passwordField;
         for (var i = 0; passwordField = passwordFields[i]; i++)
             passwordField.value = password;
-    }, false);
+            
+        processFrames(doc, password);
+    }
+    
+    function processFrames(doc, password)
+    {
+        var frames = doc.querySelectorAll('frame,iframe'),
+            frame,
+            frameDoc;
+        for (var i = 0; frame = frames[i]; i++)
+        {
+            try
+            {
+                frameDoc = frame.contentDocument;
+                if (frameDoc)
+                    fillForms(frameDoc, password);
+            }
+            catch(e)
+            {
+                continue;
+            }
+        }
+    }
 }, false);
