73abc6c4b8636d92ee51af987914871173e663a8
Rafa? Rutkowski <rrutkowski@gmail.com>
2013-09-30 23:44:39 +0200

    Ported to Chrome.

---
+++ crx/js/popup.js
@@ -0,0 +1,31 @@
+window.addEventListener("load", function() {
+
+    function getActiveTab(callback) {
+        chrome.tabs.query({
+            active: true,
+            currentWindow: true
+        }, function(tabs) {
+            if (tabs.length > 0) {
+                callback(tabs[0]);
+            }
+        });
+    }
+    
+    var form = document.getElementById("form");
+
+    getActiveTab(function(tab) {
+        form.elements["domainName"].value = getDomainName(tab.url);
+    });
+
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
+        var masterPassword = form.elements["masterPassword"].value,
+            domainName = form.elements["domainName"].value,
+            password = makePassword(masterPassword, domainName, 10);
+        getActiveTab(function(tab) {
+            chrome.tabs.sendMessage(tab.id, {password: password});
+            window.close();
+        });
+    }, false);
+}, false);
+
