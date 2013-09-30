f147e05763167d220e6a31a7eb54b37d4e5ad51d
Rafał Rutkowski <rrutkowski@gmail.com>
2013-09-30 23:44:39 +0200

    Ported to Chrome.

---
+++ crx/js/content.js
@@ -0,0 +1,38 @@
+(function() {
+    chrome.runtime.onMessage.addListener(function(request) {
+        var password = request.password;
+        if (!password)
+            return;
+        fillForms(document, password);
+    });
+
+    function fillForms(doc, password)
+    {
+        var passwordFields = doc.querySelectorAll('input[type="password"]'),
+            passwordField;
+        for (var i = 0; passwordField = passwordFields[i]; i++)
+            passwordField.value = password;
+            
+        processFrames(doc, password);
+    }
+    
+    function processFrames(doc, password)
+    {
+        var frames = doc.querySelectorAll('frame,iframe'),
+            frame,
+            frameDoc;
+        for (var i = 0; frame = frames[i]; i++)
+        {
+            try
+            {
+                frameDoc = frame.contentDocument;
+                if (frameDoc)
+                    fillForms(frameDoc, password);
+            }
+            catch(e)
+            {
+                continue;
+            }
+        }
+    }
+}) ();
