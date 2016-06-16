e3099780e0c99ce67a77465c034bb9b00cd65a4a
Rafał Rutkowski <rrutkowski@gmail.com>
2016-06-16 20:03:01 +0200

    Adapt tests to TLD list changes.

--- test/pmaker_spec.js
+++ test/pmaker_spec.js
@@ -13,26 +13,22 @@ describe("Password Maker", function() {
             ["google.com?hl=pl", "google.com"],
 
             ["http://www.anathema.art.pl/forum/login.php", "anathema.art.pl"],
-            
+
             ["http://foo.example.com:1234/bar", "example.com"],
 
-            ["http://a.ba.ar", "a.ba.ar"],
-            ["http://a.uba.ar", "uba.ar"],
+            ["http://a.com.ar", "a.com.ar"],
+            ["http://a.comm.ar", "comm.ar"],
 
-            ["http://a.act.du.au", "act.du.au"],
+            ["http://a.act.du.au", "du.au"],
             ["http://a.ac.edu.au", "ac.edu.au"],
             ["http://a.act.edu.au", "a.act.edu.au"],
 
-            ["http://a.b.c.tokyo.jp", "b.c.tokyo.jp"],
-            ["http://a.b.metro.tokyo.jp", "metro.tokyo.jp"],
-
-            ["http://a.b.go.nc.tr", "go.nc.tr"],
-            ["http://a.b.go.nic.tr", "nic.tr"],
-            ["http://a.b.gov.nc.tr", "b.gov.nc.tr"],
+            ["http://a.b.kawasaki.jp", "a.b.kawasaki.jp"],
+            ["http://a.city.kawasaki.jp", "city.kawasaki.jp"],
 
             ["file://localhost/file", "localhost"]
         ];
-        
+
         for (var i = 0, l = testData.length; i < l; i++)
             expect(getDomainName(testData[i][0])).toEqual(testData[i][1]);
     });
